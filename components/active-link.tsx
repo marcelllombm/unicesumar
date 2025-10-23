f00e184b95e252c5d354d5ac993e5595ac49fa6b
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-10-22 23:49:33 -0300

    style(tailwind): update configuration with new theme colors

--- components/active-link.tsx
+++ components/active-link.tsx
@@ -4,16 +4,24 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
 type ActiveLinkProps = {
-    children: React.ReactNode;
-} &  LinkProps;
+  children: React.ReactNode;
+  className?: string;
+} & LinkProps;
 
-export default function ActiveLink({children, href, ...rest}:ActiveLinkProps){
-    const pathname = usePathname();
-    const isCurrentPath = pathname === href;
-    return(
-        <Link href={href}
-        className={cn("text-action-sm transition-colors hover:text-green-900",
-        isCurrentPath ? "text-green-900 " : "text-green-700")}
-        >{children}</Link>
-    )
+export default function ActiveLink({ children, href, className, ...rest }: ActiveLinkProps) {
+  const pathname = usePathname();
+  const isCurrentPath = pathname === href;
+  return (
+    <Link
+      href={href}
+      {...rest}
+      className={cn(
+        "text-action-sm transition-colors hover:text-green-300",
+        isCurrentPath ? "text-green-300 font-black underline" : "text-green-200 font-black",
+        className // 👈 ESSENCIAL: injeta as classes vindas do Button aqui
+      )}
+    >
+      {children}
+    </Link>
+  );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,28 @@
-import ActiveLink from "./active-link"
-import Logo from "./logo"
-import { Button } from "./ui/button"
+import ActiveLink from "./active-link";
+import Logo from "./logo";
+import { Button } from "./ui/button";
 
-export default function Header(){
-    return(
+export default function Header() {
+  return (
     <header>
-        <div >
-        <Logo/>
-        <nav className="flex gap-6">
-            <ActiveLink href='/'>Home</ActiveLink>
-            <ActiveLink href='/produtos'>Produtos</ActiveLink>
-            <ActiveLink href='/fazer-pedido'>Fazer pedido</ActiveLink>
-            <ActiveLink href='/sobre'>Sobre</ActiveLink>
-            <ActiveLink href='/contato'>Contato</ActiveLink>
-            <Button variant="secondary" asChild>
-            <ActiveLink href='/admin'>Entrar</ActiveLink>
-            </Button>
-            </nav>
-        </div>
+      <div
+        className="
+      fixed p-3 top-0 z-50 w-full border-b
+     border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60
+      flex flex-col md:flex-row md:gap-8 md:items-center md:justify-center md:mt-5"
+      >
+        <Logo />
+        <nav className="flex gap-2 items-center justify-center mt-3 md:mt-0">
+          <ActiveLink href="/">Home</ActiveLink>
+          <ActiveLink href="/produtos">Produtos</ActiveLink>
+          <ActiveLink href="/fazer-pedido">Fazer pedido</ActiveLink>
+          <ActiveLink href="/sobre">Sobre</ActiveLink>
+          <ActiveLink href="/contato">Contato</ActiveLink>
+          <Button variant="primary" asChild>
+            <ActiveLink href="/admin">Entrar</ActiveLink>
+          </Button>
+        </nav>
+      </div>
     </header>
-    )
+  );
 }
